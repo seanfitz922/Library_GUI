b9042a2e6d34d9016b033e0a2766b43d8180092a
seanfitz922 <seanfitz922@gmail.com>
2023-07-11 17:04:49 -0500

    add sort menu

--- library_gui.py
+++ library_gui.py
@@ -34,12 +34,41 @@ class LibraryGUI(tk.Tk):
         actions_menu.add_command(label="Remove", command=self.remove_book)
         menubar.add_cascade(label="Actions", menu=actions_menu)
 
+        # Create the Sort menu
+        sort_menu = tk.Menu(menubar, tearoff=0)
+
+        # submenu for title sorting
+        sort_title_submenu = tk.Menu(sort_menu, tearoff=0)
+        sort_title_submenu.add_command(label="A-Z", command=self.sort_by_title_ascending)
+        sort_title_submenu.add_command(label="Z-A", command=self.sort_by_title_descending)
+        sort_menu.add_cascade(label="Title", menu=sort_title_submenu)
+
+        # submenu for author sorting
+        sort_author_submenu = tk.Menu(sort_menu, tearoff=0)
+        sort_author_submenu.add_command(label="A-Z", command=self.sort_by_author_ascending)
+        sort_author_submenu.add_command(label="Z-A", command=self.sort_by_author_descending)
+        sort_menu.add_cascade(label="Author", menu=sort_author_submenu)
+
+        # submenu for publication date sorting
+        sort_pubdate_submenu = tk.Menu(sort_menu, tearoff=0)
+        sort_pubdate_submenu.add_command(label="Ascending", command=self.sort_by_pub_date_ascending)
+        sort_pubdate_submenu.add_command(label="Descending", command=self.sort_by_pub_date_descending)
+        sort_menu.add_cascade(label="Publication Date", menu=sort_pubdate_submenu)
+
+        # submenu for book id sorting
+        sort_id_submenu = tk.Menu(sort_menu, tearoff=0)
+        sort_id_submenu.add_command(label="Ascending", command=self.sort_by_id_ascending)
+        sort_id_submenu.add_command(label="Descending", command=self.sort_by_id_descending)
+        sort_menu.add_cascade(label="ID", menu=sort_id_submenu)
+
+        menubar.add_cascade(label="Sort", menu=sort_menu)
+
         # Configure the window to use the menu bar
         self.config(menu=menubar)
 
     def create_widgets(self):
         # Create and configure GUI components (labels, buttons, etc.)
-        self.book_listbox = tk.Listbox(self, font=("Arial", 12), height=30, width = 50)
+        self.book_listbox = tk.Listbox(self, font=("Arial", 12), height=30, width = 100)
         self.book_listbox.grid(row=0, column=0)
 
     
@@ -60,6 +89,33 @@ class LibraryGUI(tk.Tk):
         pass
 
     def remove_book(self):
+        pass
+
+    def sort_by_title_ascending(self):
+        pass
+
+    def sort_by_title_descending(self):
+        pass
+
+    def sort_by_author_ascending(self):
+        pass
+
+    def sort_by_author_descending(self):
+        pass
+
+    def sort_by_id_ascending(self):
+        pass
+
+    def sort_by_id_descending(self):
+        pass
+
+    def sort_by_pub_date_ascending(self):
+        pass
+
+    def sort_by_pub_date_descending(self):
+        pass
+    
+    def sort_pub_date(self):
         pass
 
 # Create an instance of the LibraryGUI class and run the GUI
